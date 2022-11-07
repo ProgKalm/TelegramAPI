97d6a8e9ba0f0ffffb78b0bc0ec1f693e2978f25
Ilya Suponev <suponeff.i@gmail.com>
2022-11-07 14:13:12 +0300

    update_learning dir

--- Learning/README.md
+++ Learning/README.md
@@ -3,3 +3,4 @@
 At this directory realize TelegramBot for 
 not real shop of games
 
+Learn by courses on YouTube: *https://www.youtube.com/c/PythonHubStudio*
---
+++ leraning_bot_run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call %~dp0venv\Scripts\activate
+
+cd %~dp0Learning
+
+set TOKEN=5682682178:AAELtBNb6YfuiX5PE763En0HvXJVgKOTQWQ
+
+python main.py
+
+pause
